2c132bedc9ba9c4139d39903e23b6870f30e3506
MrPoketes <az778@york.ac.uk>
2021-08-10 20:20:49 +0300

    Add drawable canvas to frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import './App.css';
-
+import CanvasDraw from 'react-canvas-draw';
+import { useState } from 'react';
 function App() {
-	return <div className="App"></div>;
+	const [canvas, setCanvas]: any = useState(null);
+
+	return (
+		<div
+			className="App"
+			style={{ justifyContent: 'center', display: 'flex', marginTop: '2rem' }}
+		>
+			<div style={{ borderColor: 'gray', borderStyle: 'solid' }}>
+				<CanvasDraw
+					hideGrid
+					hideInterface
+					ref={canvasDraw => setCanvas(canvasDraw)}
+				/>
+			</div>
+			<div>
+				<button type="button" onClick={() => canvas.clear()}>
+					Clear
+				</button>
+				<button type="button">Submit</button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
